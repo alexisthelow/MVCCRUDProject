dc5b54b7d43fee0750a5ef351f5f85c74a2ffa1a
Alexis Low <alexisthelow@gmail.com>
2017-11-11 12:45:01 -0700

    initialize model attributes

--- src/main/java/com/skilldistillery/pokedex/web/PokemonController.java
+++ src/main/java/com/skilldistillery/pokedex/web/PokemonController.java
@@ -1,9 +1,13 @@
 package com.skilldistillery.pokedex.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.skilldistillery.pokedex.data.Pokemon;
 import com.skilldistillery.pokedex.data.PokemonDAO;
 
 @Controller
@@ -13,6 +17,22 @@ public class PokemonController {
 	@Autowired
 	private PokemonDAO dao;
 
+	@ModelAttribute("activePokemon")
+	public Pokemon initActivePokemon() {
+		return dao.getPokemonById(1);
+	}
+	
+	@ModelAttribute("activePokemon")
+	public List<Pokemon> initActiveList() {
+		return dao.getAllPokemon();
+	}
+	
+	@ModelAttribute("userTeam")
+	public List<Pokemon> initUserTeam() {
+		return dao.getAllPokemonOnTeam();
+	}
+	
+	
 	
 	
 	
