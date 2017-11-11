a07ae2aadc5eb71ef5f1011e691df544d573d0d1
Alexis Low <alexisthelow@gmail.com>
2017-11-11 10:38:20 -0700

    altered fields & constructor, added gets/sets, equals, hashcode, to string

--- src/main/java/com/skilldistillery/pokedex/data/Pokemon.java
+++ src/main/java/com/skilldistillery/pokedex/data/Pokemon.java
@@ -7,8 +7,9 @@ public class Pokemon {
 	private String nickname;
 	private Type type1;
 	private Type type2;
-	private int evolutionStage;
-	private Pokemon[] evolutionChain;
+	private int evolutionStageInChain;
+	private int evolutionChainID;
+	private String description;
 	private Move move1;
 	private Move move2;
 	private Move move3;
@@ -17,22 +18,153 @@ public class Pokemon {
 	//constructors
 	public Pokemon() {}
 
-	public Pokemon(int id, String name, String nickname, Type type1, Type type2, int evolutionStage,
-			Pokemon[] evolutionChain, Move move1, Move move2, Move move3, Move move4) {
+	public Pokemon(int id, String name, Type type1, Type type2, int evolutionStageInChain,
+			int evolutionChainID, String description) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.nickname = nickname;
 		this.type1 = type1;
 		this.type2 = type2;
-		this.evolutionStage = evolutionStage;
-		this.evolutionChain = evolutionChain;
+		this.evolutionStageInChain = evolutionStageInChain;
+		this.evolutionChainID = evolutionChainID;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public Type getType1() {
+		return type1;
+	}
+
+	public void setType1(Type type1) {
+		this.type1 = type1;
+	}
+
+	public Type getType2() {
+		return type2;
+	}
+
+	public void setType2(Type type2) {
+		this.type2 = type2;
+	}
+
+	public int getEvolutionStageInChain() {
+		return evolutionStageInChain;
+	}
+
+	public void setEvolutionStageInChain(int evolutionStageInChain) {
+		this.evolutionStageInChain = evolutionStageInChain;
+	}
+
+	public int getEvolutionChainID() {
+		return evolutionChainID;
+	}
+
+	public void setEvolutionChainID(int evolutionChainID) {
+		this.evolutionChainID = evolutionChainID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Move getMove1() {
+		return move1;
+	}
+
+	public void setMove1(Move move1) {
 		this.move1 = move1;
+	}
+
+	public Move getMove2() {
+		return move2;
+	}
+
+	public void setMove2(Move move2) {
 		this.move2 = move2;
+	}
+
+	public Move getMove3() {
+		return move3;
+	}
+
+	public void setMove3(Move move3) {
 		this.move3 = move3;
+	}
+
+	public Move getMove4() {
+		return move4;
+	}
+
+	public void setMove4(Move move4) {
 		this.move4 = move4;
 	}
-	
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pokemon other = (Pokemon) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pokemon [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", nickname=");
+		builder.append(nickname);
+		builder.append(", type1=");
+		builder.append(type1);
+		builder.append(", type2=");
+		builder.append(type2);
+		builder.append(", description=");
+		builder.append(description);
+		builder.append("]");
+		return builder.toString();
+	}
+
 }
