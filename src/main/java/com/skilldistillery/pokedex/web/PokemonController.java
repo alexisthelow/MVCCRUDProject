7fe8f662c5a3ce151dba89047db764318a82d910
Alexis Low <alexisthelow@gmail.com>
2017-11-11 17:34:55 -0700

    mapping for single pokemon detail

--- src/main/java/com/skilldistillery/pokedex/web/PokemonController.java
+++ src/main/java/com/skilldistillery/pokedex/web/PokemonController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -40,6 +41,17 @@ public class PokemonController {
 			@ModelAttribute("activePokemon") Pokemon activePokemon,
 			@ModelAttribute("userTeam") List<Pokemon> userTeam) {
 		ModelAndView mv = new ModelAndView("index");
+		
+		return mv;
+	}
+	
+	@RequestMapping(path = "showOnePokemonDetail.do", method = RequestMethod.GET, params = "id")
+	public ModelAndView showOnePokemonDetail(@RequestParam("id") int id,
+			@ModelAttribute("activePokemon") Pokemon pkmn,
+			@ModelAttribute("activeList") List<Pokemon> activeList) {
+		ModelAndView mv = new ModelAndView("details");
+		mv.addObject("activePokemon", dao.getPokemonById(id));
+		
 		
 		return mv;
 	}
